12976080c731b0a912a9fc6e7fbc6781925e086c
Barathsriram <133561863+BarathSriram1270@users.noreply.github.com>
2025-03-23 11:48:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home, Feed, Dashboard, Create } from "./pages"
+import { Home, Feed, Dashboard, Create } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/employer" >
-          <Route path="/employer/dashboard" element={<Dashboard />}/>
-          <Route path="/employer/create" element={<Create />}/>
-          </Route>
-          <Route path="/employee/feed" element={<Feed />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/employer/dashboard" element={<Dashboard />} />
+        <Route path="/employer/create" element={<Create />} />
+        <Route path="/employee/feed" element={<Feed />} />
       </Routes>
     </BrowserRouter>
   );
